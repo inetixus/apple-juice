921d950527013277e9b72f898b07ed92b4023b3f
inetixus <inetixus@gmail.com>
2026-04-22 16:51:12 +0200

    chore: ignore build artifacts (.next, node_modules, .vercel, .turbo)

--- .next/server/app/api/chat/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/chat/route.js")
-R.c("server/chunks/[root-of-the-server]__0r~65-q._.js")
-R.c("server/chunks/[root-of-the-server]__0op9af~._.js")
-R.c("server/chunks/_next-internal_server_app_api_chat_route_actions_0tmcf6..js")
-R.m(99280)
-module.exports=R.m(99280).exports
